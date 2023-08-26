7f25c598cb059d5c0c1dde37280fef16a75b8f84
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-08-26 16:15:20 -0400

    feat: added "under construction banner"

--- frontend/arepas/src/App.jsx
+++ frontend/arepas/src/App.jsx
@@ -1,5 +1,23 @@
 import "./App.css";
 
+function Banner() {
+  return (
+    <div className="w-full p-5 bg-red-200" >
+      
+      <div className="font-bold text-lg text-black text-center">
+         This site is currently under construction. Please check back soon!
+
+      </div>
+      
+
+     </div> 
+      
+      
+
+  );
+}
+
+
 function Hero() {
   return (
     <div className="bg-white">
@@ -175,6 +193,7 @@ function Footer() {
 function App() {
   return (
     <>
+      <Banner />
       <Hero />
       <SectionBreak />
       <About />
