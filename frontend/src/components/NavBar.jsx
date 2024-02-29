13ee148ec0b8ab911a196a76e9c79f3606247446
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-02-29 03:18:32 -0500

    feat: working cart

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
-// Import Babel
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 // NavBar Component
 
@@ -7,17 +7,23 @@ export default function NavBar() {
   return (
     <div className="py-2 ">
       <div className="container flex items-center justify-between">
-        <div className="flex items-center">
-          <div className="h-10 w-5 bg-[#F8C700]"></div>
-          <div className="h-10 w-3 bg-[#002E83]"></div>
-          <div className="h-10 w-3 bg-[#C30E2D]"></div>
-          <h1 className="ml-5 font-bold">Rexburg Arepas</h1>
-        </div>
+        <a href="/">
+          <div className="flex items-center">
+            <div className="h-10 w-5 bg-[#F8C700]"></div>
+            <div className="h-10 w-3 bg-[#002E83]"></div>
+            <div className="h-10 w-3 bg-[#C30E2D]"></div>
+            <h1 className="ml-5 font-bold">Rexburg Arepas</h1>
+            <div className="hidden lg:block">
+              <TimeSchedule />
+            </div>
+          </div>
+        </a>
+
         <nav className="flex items-center gap-8">
-          <a href="#" class="link-hover link hidden md:block">
+          <a href="#" className="link-hover link hidden md:block">
             About
           </a>
-          <a href="#" class="link-hover link hidden md:block">
+          <a href="#" className="link-hover link hidden md:block">
             Pick Up
           </a>
           <a href="#">
@@ -25,6 +31,34 @@ export default function NavBar() {
           </a>
         </nav>
       </div>
+    </div>
+  );
+}
+
+function TimeSchedule() {
+  // Check if store is open.
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const date = new Date();
+    const day = date.getDay();
+    const hour = date.getHours();
+
+    if (day === 2 || day === 4) {
+      if (hour >= 12 && hour <= 18) {
+        setIsOpen(true);
+      }
+    }
+  });
+
+  return (
+    <div className="flex items-center gap-4">
+      {isOpen ? (
+        <div className="badge badge-success mx-4">Open</div>
+      ) : (
+        <div className="badge badge-error mx-4">Closed</div>
+      )}
+      <div className=" font-thin">Tue & Thur: 12 PM - 6 PM</div>
     </div>
   );
 }
--- frontend/src/components/Order.jsx
+++ frontend/src/components/Order.jsx
@@ -1,29 +1,61 @@
+"use client";
+
+import React, { useState } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa";
 
-export default function PickUp() {
+export default function OrderCreation() {
+  const [order, setOrder] = useState({});
+  const [total, setTotal] = useState(0);
+
+  const updateOrder = (item, quantity) => {
+    const newOrder = { ...order };
+    if (quantity > 0) {
+      newOrder[item.name] = { ...item, quantity };
+    } else {
+      delete newOrder[item.name];
+    }
+    setOrder(newOrder);
+
+    const newTotal = Object.values(newOrder).reduce(
+      (acc, currentItem) => acc + currentItem.price * currentItem.quantity,
+      0,
+    );
+    setTotal(newTotal);
+  };
   return (
     <section className="w-full py-16 ">
       <div className="container flex flex-col md:flex-row">
         <div className="grid grid-cols-2 gap-1 md:w-1/2 md:gap-8">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {MenuItems.map((item) => (
+            <Card key={item.name} item={item} updateOrder={updateOrder} />
+          ))}
         </div>
         <div className="mt-8 flex flex-col md:mt-0 md:w-1/2 md:p-10 ">
           <h2 className="text-3xl">Order</h2>
           <p className="my-6">
-            Every order is made fresh and ready to pick up in 20 minutes. Each
-            order comes with 3 arepas.
+            Ready to pick up in 20 minutes. Each order comes with 3 arepas.
           </p>
 
-          <div className="my-4 flex flex-col justify-between rounded-lg border p-8 ">
-            <div className="flex min-h-[100px] flex-col justify-center">
-              No Items. Add some to your cart.
+          <div className="my-4 flex flex-col justify-between rounded-lg border bg-base-100 p-8 ">
+            <div
+              id="cart"
+              className="flex min-h-[100px] flex-col justify-center"
+            >
+              {Object.keys(order).length === 0 ? (
+                "No Items. Add some to your cart."
+              ) : (
+                <ul>
+                  {Object.values(order).map((item) => (
+                    <li key={item.name}>
+                      x{item.quantity} {item.name}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
-            <div className="flex justify-between">
+            <div className="mt-8 flex justify-between font-bold">
               <div>Total:</div>
-              <div>$0.00</div>
+              <div>${total.toFixed(2)}</div>
             </div>
           </div>
           <div className="flex gap-8">
@@ -66,17 +98,33 @@ export default function PickUp() {
   );
 }
 
-function Card() {
+function Card({ item, updateOrder }) {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleIncrement = () => {
+    const newQuantity = quantity + 1;
+    setQuantity(newQuantity);
+    updateOrder(item, newQuantity);
+  };
+
+  const handleDecrement = () => {
+    const newQuantity = Math.max(0, quantity - 1);
+    setQuantity(newQuantity);
+    updateOrder(item, newQuantity);
+  };
   return (
     <div className="flex flex-col items-center justify-center gap-4 rounded-2xl bg-white p-8 shadow-md">
-      <img src="/arepa_chicken.png" />
-      <div>Arepa with Chicken</div>
+      <img src={item.image} alt={item.name} />
+      <span className="absolute flex h-[50px] w-[75px] -translate-x-[75px] transform items-center justify-center rounded-lg bg-[#002E83] text-white ">
+        ${item.price}
+      </span>
+      <div>{item.name}</div>
       <div className="flex items-center">
-        <button class="btn btn-circle btn-sm">
+        <button className="btn btn-circle btn-sm" onClick={handleDecrement}>
           <FaMinus />
         </button>
-        <div className="w-14 text-center">0</div>
-        <button className="btn btn-circle btn-sm">
+        <div className="w-14 text-center">{quantity}</div>
+        <button className="btn btn-circle btn-sm" onClick={handleIncrement}>
           <FaPlus />
         </button>
       </div>
@@ -84,37 +132,25 @@ function Card() {
   );
 }
 
-function PiUp() {
-  return (
-    <section className="w-full bg-[#F8C700] py-16">
-      <div className="container flex flex-col items-center">
-        <div></div>
-        <div>
-          <h2 className="text-3xl">Pick Up</h2>
-          <p className="py-6">
-            Every order is made fresh and ready to pick up in 20 minutes. Each
-            order comes with 3 arepas.
-          </p>
-          <button className="btn btn-primary px-10">Open Map</button>
-        </div>
-      </div>
-    </section>
-  );
-}
-
-function PickU() {
-  return (
-    <section className="w-full bg-[#F8C700] py-16">
-      <div className="container flex flex-col items-center">
-        <h2 className="text-3xl">Pick Up</h2>
-        <p className="py-6">
-          Arepas are a staple of Colombian cuisine. They are a type of food made
-          of ground maize dough, originating from the northern region of South
-          America in pre-Columbian times, and are notable in the cuisines of
-          Colombia and Venezuela.
-        </p>
-        <button className="btn btn-primary px-10">Open Map</button>
-      </div>
-    </section>
-  );
-}
+const MenuItems = [
+  {
+    name: "Arepas with Chicken & Salsa",
+    price: 13,
+    image: "/arepa_chicken_salsa.png",
+  },
+  {
+    name: "Arepas with Chicken",
+    price: 12,
+    image: "/arepa_chicken.png",
+  },
+  {
+    name: "Arepas with Salsa",
+    price: 11,
+    image: "/arepa_salsa.png",
+  },
+  {
+    name: "Arepas Plain",
+    price: 10,
+    image: "/arepa_plain.png",
+  },
+];
