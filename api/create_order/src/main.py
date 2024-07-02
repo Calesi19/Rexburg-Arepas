63f3a4247051f8665cb2d639d0abda59c50f0f91
Carlos Lespin <clespin@millwork.dev>
2024-07-02 16:34:18 -0500

    fix: clean up. working endpoint

--- api/create_order/src/main.py
+++ api/create_order/src/main.py
@@ -13,23 +13,13 @@ def main(context):
     client.set_key(os.environ["APPWRITE_API_KEY"]) # Your secret API key
 
     database = Databases(client)
-
-    context.log("Project Id: " + os.environ["APPWRITE_PROJECT_ID"])
-    context.log("Project Id: " + os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    context.log("API KEY: " + os.environ["APPWRITE_API_KEY"])
-    context.log("Collection Id: " + os.environ["APPWRITE_COLLECTION_ID"])
-    context.log("Database Id: " + os.environ["APPWRITE_DATABASE_ID"])
-    context.log("Connected to Appwrite Database")
-    
+   
     # Get Orders From Database
     try:
-        colle_id = os.environ["APPWRITE_COLLECTION_ID"]
-        db_id = os.environ["APPWRITE_DATABASE_ID"]
+        collection_id = os.environ["APPWRITE_COLLECTION_ID"]
+        database_id = os.environ["APPWRITE_DATABASE_ID"]
 
-        documents = database.list_documents(db_id, colle_id)
-
-        context.log("Fetched Orders from Database")
-        context.log(documents)
+        documents = database.list_documents(database_id, collection_id)
 
         return context.res.json(documents)
 
