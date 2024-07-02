2c8407a530f61d39c0f0eac4081032d240312da6
Carlos Lespin <clespin@millwork.dev>
2024-07-02 16:05:13 -0500

    fix: logging

--- api/create_order/src/main.py
+++ api/create_order/src/main.py
@@ -9,11 +9,16 @@ def main(context):
     client = Client()
 
     client.set_endpoint("https://cloud.appwrite.io/v1") # Your API Endpoint
-    client.set_project(os.environ["APPWRITE_PROJECT_ID"]) # Your project ID
+    client.set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"]) # Your project ID
     client.set_key(os.environ["APPWRITE_API_KEY"]) # Your secret API key
 
     database = Databases(client)
 
+    context.log("Project Id: " + os.environ["APPWRITE_PROJECT_ID"])
+    context.log("Project Id: " + os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+    context.log("API KEY: " + os.environ["APPWRITE_API_KEY"])
+    context.log("Collection Id: " + os.environ["APPWRITE_COLLECTION_ID"])
+    context.log("Database Id: " + os.environ["APPWRITE_DATABASE_ID"])
     context.log("Connected to Appwrite Database")
     
     # Get Orders From Database
