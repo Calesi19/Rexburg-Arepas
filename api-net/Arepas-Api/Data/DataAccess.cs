ae5ff8cea3ec5e990dd6adec5a9de9d375016f12
Carlos Lespin <clespin@millwork.dev>
2024-08-12 16:56:53 -0500

    feat: init data access not working

--- api-net/Arepas-Api/Data/DataAccess.cs
+++ api-net/Arepas-Api/Data/DataAccess.cs
@@ -1,4 +1,42 @@
+namespace DataAccess;
 
+// Data Access
 
+public class DataAccess
+{
+    public static string ConnectionString { get; set; }
 
+    public static SqlConnection GetConnection()
+    {
+        return new SqlConnection(ConnectionString);
+    }
 
+    public static DataTable GetDataTable(string query)
+    {
+        using (var connection = GetConnection())
+        {
+            connection.Open();
+            using (var command = new SqlCommand(query, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    var table = new DataTable();
+                    table.Load(reader);
+                    return table;
+                }
+            }
+        }
+    }
+
+    public static void ExecuteNonQuery(string query)
+    {
+        using (var connection = GetConnection())
+        {
+            connection.Open();
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
